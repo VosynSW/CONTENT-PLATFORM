5e40cf57d5a433af4d26e5e8bfb416fca0415812
cdhaldane <48765995+Cdhaldane@users.noreply.github.com>
2023-11-08 14:06:28 -0500

    file sturcture

--- vosyn-webapp/src/App.js
+++ vosyn-webapp/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Views/Home';
+import VideoPage from './Views/Video-Page';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/video" element={<VideoPage />} />
+    </Routes>
   );
 }
 
